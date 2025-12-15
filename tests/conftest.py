dba3c62828a32648f874958babca53eb738c8bd8
dbruce-ae05 <dbruce.ae05@gmail.com>
2025-12-14 23:20:10 -0500

    implementing base_project

---
+++ tests/conftest.py
@@ -0,0 +1,28 @@
+import pytest
+from pytest import Config, Item, Parser
+
+from src.gui_library.config import settings_generator
+
+
+def pytest_addoption(parser: Parser):
+    parser.addoption("--runslow", action="store_true", default=False, help="run slow tests")
+
+
+def pytest_configure(config: Config):
+    config.addinivalue_line("markers", "slow: mark test as slow to run")
+
+
+def pytest_collection_modifyitems(config: Config, items: list[Item]):
+    if config.getoption("--runslow"):
+        # -- runslow given in cli, do not skip slow tests
+        return
+
+    skip_slow = pytest.mark.skip(reason="need --runslow option to run")
+    for item in items:
+        if "slow" in item.keywords:
+            item.add_marker(skip_slow)
+
+
+@pytest.fixture
+def settings():
+    return settings_generator()
